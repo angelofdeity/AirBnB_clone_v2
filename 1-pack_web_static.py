cad3f484606da37f1bc67b07d1cc2304226d4722
Nico <lawani.gk@gmail.com>
2023-04-06 21:26:11 +0100

    add 2 python scripts

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module contains a script that generates a .tgz archive from the
-contents of the web_static folder of your AirBnB Clone repo, using the
+contents of the web_static folder of AirBnB Clone v2, using the
 function do_pack.
 """
 from fabric.api import local
@@ -10,13 +10,14 @@ import os
 
 def do_pack():
     """This function generates a .tgz archive from the contents of the
-    web_static folder of your AirBnB Clone repo, using the function do_pack.
+    web_static folder of AirBnB Clone v2, using the function do_pack.
     """
-    if not os.path.exists("versions"):
-        if local("mkdir versions").failed:
-            return None
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    file = f"versions/web_static_{date}.tgz"
-    if local(f"tar -cvzf {file} ./web_static").failed:
+    try:
+        if not os.path.exists('versions'):
+            local('mkdir versions')
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        file_name = f"versions/web_static_{date}.tgz"
+        local(f"tar -cvzf {file_name} ./web_static")
+        return file_name
+    except Exception:
         return None
-    return file
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
-"""This script"""
+"""This script distributes an archive to web servers, using the function
+do_deploy"""
+import os
+from fabric.api import run, env, cd, put
+env.hosts = ['54.208.84.31', '54.172.141.180']
+env.user = "ubuntu"
+
+
+def do_deploy(archive_path):
+    """This function distributes an archive to web servers"""
+    if (os.path.exists(archive_path) is False):
+        return False
+    filename = f'/data/web_static/releases/{archive_path.strip(".tgz")}'
+    archive = archive_path.lstrip('versions/')
+    run(f'mkdir -p {filename}')
+    # run('mkdir /tmp/versions')
+    put(f'{archive_path}', "/tmp/")
+    run(f'tar -xzf /tmp/{archive} -C {filename}')
+    run(f'mv {filename}/web_static/* {filename}')
+    run(f'rm -rf {filename}/web_static/')
+    run(f'rm /tmp/{archive}')
+    run('rm /data/web_static/current')
+    run(f'ln -sf {filename} /data/web_static/current')
+    return True
